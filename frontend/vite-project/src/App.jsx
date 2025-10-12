2fade9d13563b87ba0e61a0b37ffbecde088c210
mts-mlna <matiasezequielmolina2006@gmail.com>
2025-10-11 21:06:19 -0300

    realizacion de un encabezado

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router"
 import Asistencia from "./components/Asistencia"
 import Header from "./components/Header"
+import Home from "./components/Home"
 
 function App() {
   return (
     <BrowserRouter>
     <div>
       <Header />
+      <Home />
     </div>
     </BrowserRouter>
   )
---
+++ frontend/vite-project/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+function Home() {
+  return (
+    <main className='bg-white w-full'>
+
+    </main>
+  )
+}
+
+export default Home
