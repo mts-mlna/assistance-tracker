472bbc0aa0d7c86fe0ae8edf0f91b37613f517a6
mts-mlna <matiasezequielmolina2006@gmail.com>
2025-10-24 22:09:53 -0300

    inicio desde cero con node y react

--- backend/project/app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from app.models import *
-
-# Register your models here.
-admin.site.register(alumnos)
--- frontend/vite-project/src/api/asistencia.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const asistenciaApi = axios.create({
-    baseURL: 'http://localhost:8000/api/asistencia/',
-})
-
-export const getAsistencia = () => asistenciaApi.get()
