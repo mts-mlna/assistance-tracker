14e201c229f76860d4b84d50ce7dbe261e4e9521
Bruno Ezequiel Sánchez <3zequielbsanchez@gmail.com>
2025-11-05 13:59:22 -0300

    Correccion de la Base de datos

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,22 +1,31 @@
+
 const SQLite3 = require('sqlite3')
-const ruta = require('path')
-const SQLite3_location = ruta.resolve(__dirname,'./db.db')
 
-const db_create = new SQLite3.Database(SQLite3_location,(Error)=>{
-    if(Error){
-        console.error('Error al crear la base de datos')
-    }
-    else{
-        console.log('Base de datos creada correctamente')
-        db_create.run(
-            // TODO: Completar abajo con las tablas y columnas que hagan falta
-            `
-                CREATE TABLE IF NOT EXISTS Alumnos(
-                    
-                )
-            `
-        )
-    }
-})
+const Ruta = require('path');
 
-module.exports = db_create;
+const SQLite3_Ubicacion = Ruta.resolve(__dirname, './DataBase.db');
+
+const db_crear = new SQLite3.Database(SQLite3_Ubicacion, (error) => {
+    if (error) {
+        console.error('❌ Error al conectar con la base de datos:', error.message);
+    } else {
+        console.log('🛠️ Se ha creado la Base De Datos');
+        db_crear.run(`
+            CREATE TABLE IF NOT EXISTS Usuario (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Nombre TEXT NOT NULL,
+            Correo TEXT NOT NULL UNIQUE,
+            Contraseña TEXT NOT NULL,
+            Rol TEXT NOT NULL
+        )`, (error) => {
+            if (error) {
+                console.error('❌ Error al crear la tabla Usuario:', error.message);
+            } else {
+                console.log('✅ Tabla Usuario creada correctamente');
+            }
+        }
+        );
+    }
+});
+
+module.exports = db_crear;
